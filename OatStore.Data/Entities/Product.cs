4b60d72d612b8bb4b8eabc35447d11b97d4f6009
ORabpornpra <ORabpornpra@blendtec.com>
2013-03-07 23:28:34 -0700

    Close #3: Added Product Model

---
+++ OatStore.Data/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OatStore.Data.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public String Name { get; set; }
+
+        public String Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+    }
+}
